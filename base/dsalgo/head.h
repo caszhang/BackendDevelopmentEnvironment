0fa87325d1548ff51c790d15869c37302b933c0c
caszhang <80176975@qq.com>
2014-01-15 18:57:45 +0800

    new file:   head.h

---
+++ base/dsalgo/head.h
@@ -0,0 +1,52 @@
+// Copyright (c) 2013, Baidu Inc.
+// All rights reserved.
+// Author: zhangguoqiang01 <zhangguoqiang01@baidu.com>
+// All common head files
+#ifndef HEAD_H_
+#define HEAD_H_
+//c program std lib
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+
+//c++ std lib
+#include <iostream>
+#include <string>
+#include <stack>
+#include <queue>
+#include <map>
+#include <vector>
+#include <set>
+#include <list>
+#include <utility>
+#include <iostream>
+#include <fstream>
+
+//linux system lib
+#include <sys/time.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/file.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <assert.h>
+#include <limits.h>
+#include <signal.h>
+#include <memory.h>
+#include <sys/resource.h>
+#include <pthread.h>
+
+//network 
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+
+#endif // HEAD_H_
